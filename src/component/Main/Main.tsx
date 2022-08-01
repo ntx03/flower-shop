196edd96d96df8fc2b2c512f56cff23a887c6355
Andrey Kachur <ntx033@yandex.ru>
2022-08-01 16:13:20 +0500

    slider main page

--- src/component/Main/Main.tsx
+++ src/component/Main/Main.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Header from '../Header/Header';
-import Footer from "../Footer/Footer";
 import './Main.scss';
 import 'swiper/css';
 import image from '../../img/main__image.svg';
 // для главного меню 
 import { mainCardProducts, cardStock } from '../../utils/constantsMainPage';
-
 import button_left from '../../img/button_left.svg';
 import button_right from '../../img/button_right.svg';
 import stock_flowers from '../../img/stock_flower.svg';
@@ -21,9 +19,51 @@ import truck from '../../img/truck.svg';
 import CardMainProducts from "../CardMainProducts/CardMainProducts";
 import CardStock from "../CardStock/CardStock";
 import InformationItem from "../InformationItem/InformationItem";
-
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from "swiper";
 
 function Main() {
+
+    // состояние ширины экрана
+    const [width, setWidth] = React.useState(window.innerWidth);
+
+    // мониторим ширину экрана
+    React.useEffect(() => {
+        const handleResizeWindow = () => setWidth(window.innerWidth);
+        window.addEventListener("resize", handleResizeWindow);
+        return () => {
+            window.removeEventListener("resize", handleResizeWindow);
+        };
+    }, []);
+
+    const navigationPrevRef = React.useRef(null)
+    const navigationNextRef = React.useRef(null)
+
+    // установка ширины м/у карточкками в слайдере
+    function spaceBetweenMonitor() {
+        if (width > 1240) {
+            return 30;
+        }
+        else if (width > 1050) {
+            return 25;
+        }
+        else if (width > 900) {
+            return 20;
+        } else if (width > 500) {
+            return 15;
+        } else return 9;
+    }
+    // количество слайдов в контейнере
+    function MonitorSlidesPerView() {
+        if (width > 1000) {
+            return 4;
+        } else return 3;
+    }
+
     return (
         <>
             <Header />
@@ -39,14 +79,34 @@ function Main() {
                 </div>
                 <h2 className="main__title">Акции</h2>
                 <div className="main__stock">
-                    <button className="main__stock-button-left"><img src={button_left} alt="левая кнопка в виде стрелки" /></button>
-                    <div className="main__stock-container">
-                        {cardStock.map((item) => {
-                            return (<CardStock price={item.price} priseOld={item.priseOld} image={item.image} text={item.text} />)
-                        }
-                        )}
-                    </div>
-                    <button className="main__stock-button-right"><img src={button_right} alt="правая кнопка в виде стрелки" /></button>
+                    <button className="main__stock-button-left" ref={navigationPrevRef}><img src={button_left} alt="левая кнопка в виде стрелки" /></button>
+                    <Swiper spaceBetween={spaceBetweenMonitor()}
+                        slidesPerView={MonitorSlidesPerView()}
+                        modules={[Navigation]}
+                        navigation={{
+                            prevEl: navigationPrevRef.current,
+                            nextEl: navigationNextRef.current,
+                        }}
+                        onBeforeInit={(swiper) => {
+                            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                            // @ts-ignore
+                            // eslint-disable-next-line no-param-reassign
+                            swiper.params.navigation.prevEl = navigationPrevRef.current;
+                            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                            // @ts-ignore
+                            // eslint-disable-next-line no-param-reassign
+                            swiper.params.navigation.nextEl = navigationNextRef.current;
+                        }}
+                        onSlideChange={() => console.log('slide change')}
+                        onSwiper={(swiper) => console.log(swiper)} >
+                        <div className="main__stock-container">
+                            {cardStock.map((item, index) => {
+                                return (<SwiperSlide><CardStock price={item.price} priseOld={item.priseOld} image={item.image} text={item.text} key={index} /></SwiperSlide>)
+                            }
+                            )}
+                        </div>
+                    </Swiper>
+                    <button className="main__stock-button-right" ref={navigationNextRef}><img src={button_right} alt="правая кнопка в виде стрелки" /></button>
                 </div>
                 <h2 className="main__title">Заказать уникальный букет</h2>
                 <form className="main__form">
